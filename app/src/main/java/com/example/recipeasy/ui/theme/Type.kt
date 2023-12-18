ea7c285a59b836ecd47945ebe02bce38a8e7a849
clint.kindermans <clint.kindermans@student.howest.be>
2023-12-18 21:30:37 +0100

    specified the fonts

--- app/src/main/java/com/example/recipeasy/ui/theme/Type.kt
+++ app/src/main/java/com/example/recipeasy/ui/theme/Type.kt
@@ -2,18 +2,35 @@ package com.example.recipeasy.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.recipeasy.R
+
+val javaneseText = FontFamily(
+    Font(R.font.javatext)
+)
+
+val inter = FontFamily(
+    Font(R.font.inter)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = inter,
+        fontWeight = FontWeight.ExtraLight,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = javaneseText,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
