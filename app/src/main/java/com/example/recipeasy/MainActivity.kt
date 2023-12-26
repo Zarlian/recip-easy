249f540c9b6dbbb5781a299a16631f61faf32728
clint.kindermans <clint.kindermans@student.howest.be>
2023-12-26 14:10:53 +0100

    fixed bug

--- app/src/main/java/com/example/recipeasy/MainActivity.kt
+++ app/src/main/java/com/example/recipeasy/MainActivity.kt
@@ -40,6 +40,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -122,6 +123,7 @@ fun NavBarItems(
     Text(
         text = stringResource(text),
         style = MaterialTheme.typography.bodyLarge,
+        fontWeight = if(selectedPage == page)  FontWeight.ExtraBold else FontWeight.Normal,
         color = if (selectedPage == page) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary,
         modifier = modifier
             .padding(horizontal = 8.dp)
