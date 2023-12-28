7ce1fd48004905fc3e693636136d690589502f8a
clint.kindermans <clint.kindermans@student.howest.be>
2023-12-28 11:39:48 +0100

    made pantry work with proper dummy data

---
+++ app/src/main/java/com/example/recipeasy/data/dataclasses/PantryItem.kt
@@ -0,0 +1,10 @@
+package com.example.recipeasy.data.dataclasses
+
+import androidx.compose.ui.graphics.Color
+
+data class PantryItem(
+    val name: String,
+    val quantity: Int,
+    val image: Int,
+    val color: Color
+)
--- app/src/main/java/com/example/recipeasy/ui/theme/pages/PantryPage.kt
+++ app/src/main/java/com/example/recipeasy/ui/theme/pages/PantryPage.kt
@@ -1,4 +1,3 @@
-import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -24,66 +23,44 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.recipeasy.Page
-import com.example.recipeasy.R
+import com.example.recipeasy.data.dataclasses.PantryItem
+import com.example.recipeasy.data.pantryItems
 import com.example.recipeasy.ui.theme.RecipeasyTheme
 
 @Composable
-fun PantryPage(modifier: Modifier = Modifier, selectedPage: Page, onItemSelected: (Page) -> Unit) {
+fun PantryPage(
+    modifier: Modifier = Modifier,
+    selectedPage: Page, onItemSelected: (Page) -> Unit,
+    pantry: List<PantryItem>) {
     Column(modifier = modifier.fillMaxSize()) {
         Header(
             selectedPage = selectedPage,
             onItemSelected = onItemSelected,
             modifier = modifier
         )
-        PantryList(modifier = modifier.align(Alignment.CenterHorizontally))
+        PantryList(modifier = modifier.align(Alignment.CenterHorizontally), pantry = pantry)
     }
 }
 
 @Composable
-fun PantryList(modifier: Modifier = Modifier) {
-    val ingredientsStrings = listOf(
-        "Beans",
-        "Chicken Breast",
-        "Eggs",
-        "Honey",
-        "Onions",
-        "Tagliatelle"
-    )
-    val ingredientDrawableMap = mapOf(
-        "Beans" to R.drawable.beans,
-        "Chicken Breast" to R.drawable.chicken_breast,
-        "Eggs" to R.drawable.eggs,
-        "Honey" to R.drawable.honey,
-        "Onions" to R.drawable.onions,
-        "Tagliatelle" to R.drawable.tagliatelle
-    )
-
-    val colorList = listOf(
-        0xFFEECED3,
-        0xFFBEDEDF,
-        0xFFF7CE94,
-        0xFFF7E7A9
-    )
-    var colorIndex = 0
+fun PantryList(modifier: Modifier = Modifier, pantry: List<PantryItem>) {
 
     Box(modifier.fillMaxSize(),
         contentAlignment = Alignment.Center ){
         LazyColumn(
-            modifier = Modifier.padding(bottom = 72.dp), // Adjust bottom padding for FAB height
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            items(25) { index ->
+            items(pantry.size) {index ->
+                val pantryItem = pantry[index]
                 PantryArticle(
-                    text = ingredientsStrings[index % ingredientsStrings.size],
-                    drawable = ingredientDrawableMap[ingredientsStrings[index % ingredientsStrings.size]]
-                        ?: R.drawable.default_image,
-                    color = colorList[colorIndex % colorList.size]
+                    text = pantryItem.name,
+                    drawable = pantryItem.image,
+                    color = pantryItem.color,
+                    quantity = pantryItem.quantity
                 )
-                colorIndex++
             }
 
         }
@@ -103,7 +80,7 @@ fun PantryList(modifier: Modifier = Modifier) {
 
 
 @Composable
-fun PantryArticle(text: String, drawable: Int, color: Long, modifier: Modifier = Modifier) {
+fun PantryArticle(text: String, drawable: Int, color: Color, modifier: Modifier = Modifier, quantity: Int) {
     Row(
         modifier = modifier
             .width(320.dp)
@@ -118,7 +95,7 @@ fun PantryArticle(text: String, drawable: Int, color: Long, modifier: Modifier =
             PantryImage(drawable, color)
             PantryText(text)
         }
-        PantryButton(text = R.string.number_1)
+        PantryButton(text = quantity)
     }
 }
 
@@ -132,11 +109,11 @@ private fun PantryText(text: String) {
 }
 
 @Composable
-private fun PantryImage(drawable: Int, color: Long) {
+private fun PantryImage(drawable: Int, color: Color) {
     Surface(
         shape = MaterialTheme.shapes.small,
         shadowElevation = 4.dp,
-        color = Color(color),
+        color = color,
         modifier = Modifier
             .padding(vertical = 8.dp)
             .width(130.dp)
@@ -157,7 +134,7 @@ private fun PantryImage(drawable: Int, color: Long) {
 }
 
 @Composable
-fun PantryButton(@StringRes text: Int, modifier: Modifier = Modifier) {
+fun PantryButton(text: Int, modifier: Modifier = Modifier) {
     Surface(
         shape = MaterialTheme.shapes.extraSmall,
         shadowElevation = 4.dp,
@@ -173,7 +150,7 @@ fun PantryButton(@StringRes text: Int, modifier: Modifier = Modifier) {
             ) {
                 PantryButtonText(text = "+")
                 Text(
-                    text = stringResource(text),
+                    text = text.toString(),
                     style = MaterialTheme.typography.bodyLarge,
                     color = MaterialTheme.colorScheme.primary
                 )
@@ -196,6 +173,6 @@ private fun PantryButtonText(text: String) {
 @Composable
 fun PantryListPreview() {
     RecipeasyTheme(dynamicColor = false) {
-        PantryList()
+        PantryList(pantry = pantryItems)
     }
 }
