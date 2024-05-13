78f8108e2484f95331e33b6a2d13e43388d00633
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-13 20:18:47 +0200

    changed recipe article (dummy data)

--- app/src/main/java/com/example/recipeasy/data/dataclasses/RecipeArticle.kt
+++ app/src/main/java/com/example/recipeasy/data/dataclasses/RecipeArticle.kt
@@ -3,30 +3,30 @@ package com.example.recipeasy.data.dataclasses
 import androidx.compose.ui.graphics.Color
 
 data class RecipeArticle(
+    val id: Int,
     val title: String,
     val image: Int,
     val color: Color = Color(0xFFEECED3),
-    val recipe: Recipe = Recipe(
+    val recipe: RecipeDummy = RecipeDummy(
         time = 20,
         recipeTitle = "Chicken with vegetables",
         difficulty = "Easy",
         servings = 4,
         ingredients = listOf(
-            Ingredient("Chicken", 400, "gr"),
-            Ingredient("Potatoes", 1, "kg"),
-            Ingredient("Carrots", 1, "kg"),
-            Ingredient("Onions", 1, "kg"),
-            Ingredient("Garlic", 1, "kg"),
-            Ingredient("Olive oil", 1, "tbps"),
-            Ingredient("Salt", 1, "kg"),
-            Ingredient("Pepper", 1, "kg"),
+            IngredientDummy("Chicken", 400, "gr"),
+            IngredientDummy("Potatoes", 1, "kg"),
+            IngredientDummy("Carrots", 1, "kg"),
+            IngredientDummy("Onions", 1, "kg"),
+            IngredientDummy("Garlic", 1, "kg"),
+            IngredientDummy("Olive oil", 1, "tbps"),
+            IngredientDummy("Salt", 1, "kg"),
+            IngredientDummy("Pepper", 1, "kg"),
         ),
-        steps = listOf(
-            PrepStep("Preheat oven to 200°C."),
-            PrepStep("Cut the chicken into pieces and season with salt and pepper."),
-            PrepStep("Cut the potatoes, carrots, onions and garlic into pieces."),
-            PrepStep("Place the chicken and vegetables in a baking dish and drizzle with olive oil."),
-            PrepStep("Bake for 30 minutes."),
-        )
+        preparation =   "Preheat oven to 200°C. " +
+                        "Cut the chicken into pieces and season with salt and pepper."+
+                        "Cut the potatoes, carrots, onions and garlic into pieces." +
+                        "Place the chicken and vegetables in a baking dish and drizzle with olive oil." +
+                        "Bake for 30 minutes."
+
     )
 )
