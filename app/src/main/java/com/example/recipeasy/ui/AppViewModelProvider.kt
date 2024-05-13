1899564b483d50ffb0457cbbe18029c358730049
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-13 19:48:09 +0200

    made AppViewModelProvider

---
+++ app/src/main/java/com/example/recipeasy/ui/AppViewModelProvider.kt
@@ -0,0 +1,20 @@
+package com.example.recipeasy.ui
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.recipeasy.RecipeApplication
+import com.example.recipeasy.ui.home.HomeViewModel
+
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(recipeasyApplication().container.recipeRepository)
+        }
+    }
+}
+
+
+fun CreationExtras.recipeasyApplication(): RecipeApplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as RecipeApplication)
