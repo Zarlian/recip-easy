f5b44203846be428db7d13a8837fe90e0fd80598
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-13 20:08:30 +0200

    changed pantry item to be able to be added to the room database

--- app/src/main/java/com/example/recipeasy/data/dataclasses/PantryItem.kt
+++ app/src/main/java/com/example/recipeasy/data/dataclasses/PantryItem.kt
@@ -1,10 +1,14 @@
 package com.example.recipeasy.data.dataclasses
 
 import androidx.compose.ui.graphics.Color
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "pantry")
 data class PantryItem(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,
     val quantity: Int,
-    val image: Int,
-    val color: Color
+    val image: Int
 )
