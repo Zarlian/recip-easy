3d48f8174cd75a874b9d01dd5cfecda816eb6cfd
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-13 20:19:27 +0200

    integrated api  data

--- app/src/main/java/com/example/recipeasy/ui/pages/components/RecipeCardList.kt
+++ app/src/main/java/com/example/recipeasy/ui/pages/components/RecipeCardList.kt
@@ -1,5 +1,3 @@
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
@@ -7,6 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,56 +15,69 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.example.recipeasy.data.dataclasses.RecipeArticle
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
+import coil.compose.rememberAsyncImagePainter
+import com.example.recipeasy.data.dataclasses.MealDetails
+import com.example.recipeasy.ui.theme.Colors
 
 @Composable
 fun RecipeCardList(
     modifier: Modifier = Modifier,
-    recipeArticles: List<RecipeArticle>,
-    onItemClick: (RecipeArticle) -> Unit
+    recipes: List<MealDetails>,
+    onItemClick: (String) -> Unit,
 ) {
-
     LazyColumn(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        items(recipeArticles.size) { index ->
-            val recipe = recipeArticles[index]
+
+        val colors = Colors.surfaceColors
+
+        items(items = recipes,  ) { recipe ->
+            val index = recipes.indexOf(recipe)
+            val color = colors[index % colors.size]
+
             if (index % 2 == 0) {
                 RecipeArticle(
-                    text = recipe.title,
-                    drawable = recipe.image,
-                    color = recipe.color,
-                    onClick = { onItemClick(recipe) }
+                    text = recipe.strMeal,
+                    imageUrl = recipe.strMealThumb,
+                    color = color,
+                    onClick = { onItemClick(recipe.idMeal)}
                 )
             } else {
                 RecipeArticleMirror(
-                    drawable = recipe.image,
-                    text = recipe.title,
-                    color = recipe.color,
-                    onClick = { onItemClick(recipe) }
+                    text = recipe.strMeal,
+                    color = color,
+                    onClick = { onItemClick(recipe.idMeal)},
+                    imageUrl = recipe.strMealThumb
                 )
             }
         }
     }
 }
 
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RecipeArticle(
-    @DrawableRes drawable: Int,
     text: String,
     modifier: Modifier = Modifier,
-    color: Color,
-    onClick: () -> Unit
+    imageUrl: String,
+    onClick: () -> Unit,
+    color: Color
 ) {
     Surface(
         shape = MaterialTheme.shapes.small,
-        color = color,
         shadowElevation = 4.dp,
+        color = color,
         modifier = modifier
             .padding(vertical = 8.dp)
             .width(320.dp)
@@ -74,33 +86,42 @@ fun RecipeArticle(
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
+            modifier = Modifier.fillMaxSize()
         ) {
+            Spacer(modifier = Modifier.width(10.dp))
             Image(
-                painter = painterResource(drawable),
+                painter = rememberAsyncImagePainter(imageUrl),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.size(115.dp)
+                modifier = Modifier
+                    .size(115.dp)
+                    .clip(CircleShape)
             )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
             Text(
                 text = text,
                 style = MaterialTheme.typography.titleMedium,
-                textAlign = TextAlign.Right,
-                modifier = Modifier.padding(horizontal = 8.dp),
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .weight(1f) // Expand to take remaining space
+                    .padding(end = 16.dp),
                 color = MaterialTheme.colorScheme.primary
             )
         }
     }
 }
 
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RecipeArticleMirror(
-    @DrawableRes drawable: Int,
     text: String,
     modifier: Modifier = Modifier,
-    color: Color,
-    onClick: () -> Unit
+    imageUrl: String,
+    onClick: () -> Unit,
+    color: Color
 ) {
     Surface(
         shape = MaterialTheme.shapes.small,
@@ -116,19 +137,30 @@ fun RecipeArticleMirror(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
+
             Text(
                 text = text,
                 style = MaterialTheme.typography.titleMedium,
-                textAlign = TextAlign.Right,
-                modifier = Modifier.padding(horizontal = 8.dp),
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .weight(1f) // Expand to take remaining space
+                    .padding(start = 16.dp),
                 color = MaterialTheme.colorScheme.primary
             )
+
+            Spacer(modifier = Modifier.width(16.dp))
             Image(
-                painter = painterResource(drawable),
+                painter = rememberAsyncImagePainter(imageUrl),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.size(115.dp)
+                modifier = Modifier
+                    .size(115.dp)
+                    .clip(CircleShape)
             )
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+
         }
     }
 }
