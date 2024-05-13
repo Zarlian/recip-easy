f8f86109dc4c8a484cac726b120ffbf7815fc1c5
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-13 19:49:12 +0200

    added object of colors we are using that cant be done with the theme

--- app/src/main/java/com/example/recipeasy/ui/theme/Color.kt
+++ app/src/main/java/com/example/recipeasy/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+object Colors{
+    val surfaceColors = listOf(
+                                Color(0xFFEECED3),
+                                Color(0xFFBEDEDF),
+                                Color(0xFFF7CE94),
+                                Color(0xFFF7E7A9),
+                                Color(0xFFBFACF4),
+                                Color(0xFFA5D8D3)
+    )
+}
