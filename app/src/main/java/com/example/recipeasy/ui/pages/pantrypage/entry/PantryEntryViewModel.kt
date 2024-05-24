014fd53cba1e509e8dfb33d7b4727e4f26569925
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-24 19:42:02 +0200

    made a pantryEntryScreen viewmodel

---
+++ app/src/main/java/com/example/recipeasy/ui/pages/pantrypage/entry/PantryEntryViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.recipeasy.ui.pages.pantrypage.entry
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.recipeasy.data.repositories.PantryRepository
+import com.example.recipeasy.ui.pages.pantrypage.PantryItemDetails
+import com.example.recipeasy.ui.pages.pantrypage.PantryUiState
+import com.example.recipeasy.ui.pages.pantrypage.toItem
+
+class PantryEntryViewModel(private val pantryRepository: PantryRepository) : ViewModel() {
+
+    var pantryUiState by mutableStateOf(PantryUiState())
+        private set
+
+    fun updateUiState(pantryDetails: PantryItemDetails) {
+        pantryUiState =
+            PantryUiState(pantryItem = pantryDetails, isEntryValid = validateInput(pantryDetails))
+    }
+
+    suspend fun saveItem() {
+        if (validateInput()) {
+            pantryRepository.addItem(pantryUiState.pantryItem.toItem())
+        }
+    }
+
+    private fun validateInput(uiState: PantryItemDetails = pantryUiState.pantryItem): Boolean {
+        return with(uiState) {
+            name.isNotBlank()  && image.isNotBlank() && quantity.isNotBlank()
+        }
+    }
+}
