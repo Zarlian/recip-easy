5dc720d017b6dcfd351299d4839f0237650f9d9a
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-24 19:45:05 +0200

    added routes for pages

--- app/src/main/java/com/example/recipeasy/ui/RecipEasyApp.kt
+++ app/src/main/java/com/example/recipeasy/ui/RecipEasyApp.kt
@@ -2,11 +2,15 @@ package com.example.recipeasy.ui
 
 
 import FilterDestination
-import FilterPage
-import HomePage
+import FilterScreen
+import HomeScreen
+import com.example.recipeasy.ui.pages.pantrypage.pantry.PantryDestination
+import PantryRecipeDestination
+import PantryRecipeScreen
+import com.example.recipeasy.ui.pages.pantrypage.pantry.PantryScreen
 import RecipeDetailDestination
-import RecipePage
-import com.example.recipeasy.ui.pages.ShopPage
+import RecipeScreen
+import com.example.recipeasy.ui.pages.shoppage.ShopScreen
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
@@ -15,8 +19,11 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.recipeasy.data.DataSource.defaultRecipeList
+import com.example.recipeasy.data.DataSource.pantryItems
 import com.example.recipeasy.data.DataSource.shop
-import com.example.recipeasy.ui.pages.ShopDestination
+import com.example.recipeasy.ui.pages.pantrypage.entry.PantryEntryDestination
+import com.example.recipeasy.ui.pages.pantrypage.entry.PantryEntryScreen
+import com.example.recipeasy.ui.pages.shoppage.ShopDestination
 
 
 @Composable
@@ -35,44 +42,72 @@ fun RecipEasyNavHost(
         navController = navController,
         startDestination = HomeDestination.route
     ) {
-            composable(route = HomeDestination.route) {
-                HomePage(
-                    navigateToShop = { navController.navigate(ShopDestination.route) },
-                    navigateToFilter = { navController.navigate(FilterDestination.route) },
-                    selectedPage = Page.HOME,
-                    onItemSelected = { },
-                    navigateToRecipeDetail = { navController.navigate("${RecipeDetailDestination.route}/${it}") }
+        composable(route = HomeDestination.route) {
+            HomeScreen(
+                navigateToShop = { navController.navigate(ShopDestination.route) },
+                navigateToFilter = { navController.navigate(FilterDestination.route) },
+                selectedPage = Page.HOME,
+                onItemSelected = { },
+                navigateToRecipeDetail = { navController.navigate("${RecipeDetailDestination.route}/${it}") },
+                navController = navController
 
-                )
-            }
-            composable(route = ShopDestination.route) {
-                ShopPage(
+            )
+        }
+        composable(route = ShopDestination.route) {
+            ShopScreen(
+                navigateBack = { navController.popBackStack() },
+                shop = shop
+            )
+        }
+
+        composable(route = PantryDestination.route) {
+            PantryScreen(
+                selectedPage = Page.PANTRY,
+                onItemSelected = { },
+                pantry = pantryItems,
+                navigateToFilter = { navController.navigate(FilterDestination.route) },
+                navigateToShop = { navController.navigate(ShopDestination.route) },
+                navController = navController,
+                navigateToPantryEntry = { navController.navigate(PantryEntryDestination.route) }
+            )
+        }
+
+        composable(route = PantryEntryDestination.route) {
+            PantryEntryScreen(
+                navigateBack = { navController.popBackStack() }
+            )
+        }
+
+        composable(route = PantryRecipeDestination.route) {
+            PantryRecipeScreen(
+                selectedPage = Page.RECIPES,
+                onItemSelected = { },
+                navigateToFilter = { navController.navigate(FilterDestination.route) },
+                navigateToShop = { navController.navigate(ShopDestination.route) },
+                navigateToRecipeDetail = { navController.navigate("${RecipeDetailDestination.route}/${it}") },
+                navController = navController
+            )
+        }
+
+        composable(route = FilterDestination.route) {
+            FilterScreen(
+                navigateBack = { navController.popBackStack() },
+                recipeArticles = defaultRecipeList
+            )
+        }
+
+        composable(route = RecipeDetailDestination.routeWithArgs,
+            arguments = listOf(navArgument(RecipeDetailDestination.recipeIdArg) {
+                type = NavType.StringType
+            })
+        ) {
+            val recipeId = it.arguments?.getString(RecipeDetailDestination.recipeIdArg)
+            if (recipeId != null) {
+                RecipeScreen(
                     navigateBack = { navController.popBackStack() },
-                    shop = shop
+                    recipeId = recipeId
                 )
             }
-
-            composable(route = FilterDestination.route) {
-                FilterPage(
-                    navigateBack = { navController.popBackStack() },
-                    recipeArticles = defaultRecipeList
-                )
-            }
-
-            composable( route = RecipeDetailDestination.routeWithArgs,
-                        arguments = listOf(navArgument(RecipeDetailDestination.recipeIdArg) {
-                        type = NavType.StringType
-                })
-                )
-            {
-                val recipeId = it.arguments?.getString(RecipeDetailDestination.recipeIdArg)
-                if (recipeId != null) {
-                    RecipePage(
-                        navigateBack = { navController.popBackStack() },
-                        recipeId = recipeId
-                    )
-                }
-            }
-
+        }
     }
 }
