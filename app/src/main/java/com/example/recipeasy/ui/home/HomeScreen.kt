3ad2ba39bee886dedef8bd3ffaa3ac8ba6c00f49
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-24 19:39:50 +0200

    updated screen to have a scaffold

--- app/src/main/java/com/example/recipeasy/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/recipeasy/ui/home/HomeScreen.kt
@@ -1,19 +1,19 @@
 import android.os.Build
 import androidx.annotation.RequiresExtension
 import com.example.recipeasy.ui.NavigationDestination
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
 import com.example.recipeasy.ui.api.APIUiState
 import com.example.recipeasy.ui.api.APIViewModel
 import com.example.recipeasy.ui.AppViewModelProvider
@@ -34,54 +34,94 @@ object HomeDestination : NavigationDestination {
 
 @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
 @Composable
-fun HomePage(
+fun HomeScreen(
     navigateToShop: () -> Unit,
     navigateToFilter: () -> Unit,
     modifier: Modifier = Modifier,
     selectedPage: Page,
     onItemSelected: (Page) -> Unit,
     navigateToRecipeDetail: (String) -> Unit,
+    navController: NavHostController,
 
-    viewModel : HomeViewModel = viewModel(factory = AppViewModelProvider.Factory)
+    viewModel: HomeViewModel = viewModel(factory = AppViewModelProvider.Factory)
 ) {
 
     val apiViewModel: APIViewModel = viewModel()
-    val apiUiState : APIUiState = apiViewModel.apiUiState
+    val apiUiState: APIUiState = apiViewModel.apiUiState
     val homeUiState by viewModel.homeUiState.collectAsState()
 
     LaunchedEffect(key1 = true) {
         apiViewModel.getRandomRecipes()
     }
 
-    Column(modifier = modifier.fillMaxSize()) {
-        Header(
-            selectedPage = selectedPage,
-            onItemSelected = onItemSelected,
-            onSearchClicked = navigateToFilter,
-            onShopClicked = navigateToShop
-        )
+    Scaffold(
+        topBar = {
+            MainTopBar(
+                selectedPage = selectedPage,
+                onItemSelected = onItemSelected,
+                onSearchClicked = navigateToFilter,
+                modifier = modifier,
+                onShopClicked = navigateToShop,
+                navController = navController
+            )
+        }
+    ) { innerPadding ->
         when (apiUiState) {
             is APIUiState.Success -> {
                 val recipes = apiUiState.recipes
                 RecipeCardList(
-                    modifier = modifier.align(Alignment.CenterHorizontally ),
+                    modifier = modifier
+                        .padding(innerPadding)
+                        .fillMaxWidth(),
                     recipes = recipes,
                     onItemClick = navigateToRecipeDetail,
                 )
             }
+
             is APIUiState.Loading -> Text(
                 text = "Loading recipes...",
                 modifier = Modifier.padding(all = 10.dp),
                 style = MaterialTheme.typography.bodyMedium
             )
+
             is APIUiState.Error -> Text(
                 text = "Error!",
                 modifier = Modifier.padding(all = 10.dp),
                 style = MaterialTheme.typography.bodyMedium
             )
         }
-
-
-
     }
 }
+
+//    Column(modifier = modifier.fillMaxSize()) {
+//        MainTopBar(
+//            selectedPage = selectedPage,
+//            onItemSelected = onItemSelected,
+//            onSearchClicked = navigateToFilter,
+//            onShopClicked = navigateToShop
+//        )
+//        when (apiUiState) {
+//            is APIUiState.Success -> {
+//                val recipes = apiUiState.recipes
+//                RecipeCardList(
+//                    modifier = Modifier.align(Alignment.CenterHorizontally ),
+//                    recipes = recipes,
+//                    onItemClick = navigateToRecipeDetail,
+//                )
+//            }
+//            is APIUiState.Loading -> Text(
+//                text = "Loading recipes...",
+//                modifier = Modifier.padding(all = 10.dp),
+//                style = MaterialTheme.typography.bodyMedium
+//            )
+//            is APIUiState.Error -> Text(
+//                text = "Error!",
+//                modifier = Modifier.padding(all = 10.dp),
+//                style = MaterialTheme.typography.bodyMedium
+//            )
+//        }
+//
+//
+//
+//    }
+//}
