c1f9f2099030af7c646459bfbb80f31aa62ca783
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-25 23:56:07 +0200

    added material design theme

--- app/src/main/java/com/example/recipeasy/MainActivity.kt
+++ app/src/main/java/com/example/recipeasy/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.example.recipeasy
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.recipeasy.ui.RecipEasyApp
+import com.example.recipeasy.ui.theme.AppTheme
 
-import com.example.recipeasy.ui.theme.RecipeasyTheme
+import com.example.recipeasy.ui.theme.oldtheme.RecipeasyTheme
 
 
 
@@ -20,7 +19,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RecipeasyTheme(dynamicColor = false) {
+            AppTheme(dynamicColor = false) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
