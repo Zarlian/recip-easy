aeca475a07c4f16d2d0bf54e9b0ef7e08f55c8b0
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-26 12:15:37 +0200

    added DailyRecipeWorker

---
+++ app/src/main/java/com/example/recipeasy/DailyRecipeWorker.kt
@@ -0,0 +1,32 @@
+package com.example.recipeasy
+
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.recipeasy.network.recipeApi
+
+class DailyRecipeWorker(context: Context, workerParams: WorkerParameters) : CoroutineWorker(context, workerParams) {
+
+        @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+        override suspend fun doWork(): Result {
+            val context = applicationContext
+
+            return try {
+                val recipe = recipeApi.retrofitService.getRandomRecipe()
+                val recipeName = recipe.meals[0].strMeal
+
+                val notificationText = "Try: $recipeName"
+
+                sendNotification("New recipe of the day: ", notificationText, context)
+
+                Log.d("RecipeNotificationWorker", "Recipe: $recipeName")
+                Result.success()
+            } catch (e: Exception) {
+                Log.e("WeatherNotificationWorker", "Error recipe of the day", e)
+                Result.failure()
+            }
+        }
+}
