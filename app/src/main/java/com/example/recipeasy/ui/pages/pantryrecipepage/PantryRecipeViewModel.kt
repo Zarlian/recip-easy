4af2130268c7201c477830fcc4d7ac4e44c9a1ef
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-26 17:58:26 +0200

    made viewmodel

--- app/src/main/java/com/example/recipeasy/ui/pages/pantryrecipepage/PantryRecipeViewModel.kt
+++ app/src/main/java/com/example/recipeasy/ui/pages/pantryrecipepage/PantryRecipeViewModel.kt
@@ -1,4 +1,57 @@
 package com.example.recipeasy.ui.pages.pantryrecipepage
 
-class PantryRecipeViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.recipeasy.data.dataclasses.MealDetails
+import com.example.recipeasy.data.repositories.PantryRepository
+import com.example.recipeasy.network.recipeApi
+import com.example.recipeasy.ui.pages.pantrypage.PantryUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+class PantryRecipeViewModel(pantryRepository: PantryRepository) : ViewModel() {
+
+    private val _pantryUiState: MutableStateFlow<PantryUiState> = MutableStateFlow(PantryUiState())
+
+    private val _recipesState = MutableStateFlow<List<MealDetails>>(emptyList())
+    val recipesState: StateFlow<List<MealDetails>> = _recipesState
+
+    init {
+        viewModelScope.launch {
+            pantryRepository.getMainIngredientsStream().collect { pantryItems ->
+                _pantryUiState.value = _pantryUiState.value.copy(pantryItems = pantryItems)
+            }
+        }
+
+        viewModelScope.launch {
+            pantryRepository.getMainIngredientsStream().collect { pantryItems ->
+                pantryItems.forEach {
+                    fetchIngredientRecipes(it.name)
+                }
+            }
+        }
+    }
+
+    private fun fetchIngredientRecipes(ingredient: String) {
+        viewModelScope.launch {
+            try {
+                val result = recipeApi.retrofitService.getRecipesByIngredient(ingredient)
+                if(result.meals.isNotEmpty()){
+                    addRecipesToList(result.meals)
+                }
+            } catch (e: IOException) {
+                Log.e("ERROR", "Network error: ${e.message}")
+            } catch (e: HttpException) {
+                Log.e("HTTP ERROR", "HTTP error: ${e.message}")
+            }
+        }
+    }
+
+    private fun addRecipesToList(recipes: List<MealDetails>) {
+        _recipesState.value += recipes
+    }
 }
