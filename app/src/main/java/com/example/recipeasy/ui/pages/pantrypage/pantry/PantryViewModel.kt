3b3ebd56cd22934d6fccde84ebd4e02b42b70d5d
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-26 18:00:00 +0200

    updated viewmodel for increase and decrease

--- app/src/main/java/com/example/recipeasy/ui/pages/pantrypage/pantry/PantryViewModel.kt
+++ app/src/main/java/com/example/recipeasy/ui/pages/pantrypage/pantry/PantryViewModel.kt
@@ -4,22 +4,46 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.recipeasy.data.repositories.PantryRepository
 import com.example.recipeasy.ui.pages.pantrypage.PantryUiState
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
-class PantryViewModel(pantryRepository: PantryRepository) : ViewModel() {
+class PantryViewModel(private val pantryRepository: PantryRepository) : ViewModel() {
 
-    val pantryUiState :StateFlow<PantryUiState> =
-        pantryRepository.getPantryItemsStream().map { PantryUiState(it) }
-            .stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
-                initialValue = PantryUiState()
-            )
+    private val _pantryUiState: MutableStateFlow<PantryUiState> = MutableStateFlow(PantryUiState())
+    val pantryUiState: StateFlow<PantryUiState> = _pantryUiState
 
-    companion object {
-        private const val TIMEOUT_MILLIS = 5_000L
+    init {
+        viewModelScope.launch {
+            pantryRepository.getPantryItemsStream().collect { pantryItems ->
+                _pantryUiState.value = _pantryUiState.value.copy(pantryItems = pantryItems)
+            }
+        }
+    }
+
+    fun increaseQuantity(pantryItemId: Int) {
+        viewModelScope.launch {
+            _pantryUiState.value = _pantryUiState.value.increaseQuantity(pantryItemId)
+            val pantryItem = _pantryUiState.value.pantryItems.find { it.id == pantryItemId }
+            pantryItem?.let { pantryRepository.updateItem(it) }
+        }
+    }
+
+    fun decreaseQuantity(pantryItemId: Int) {
+        viewModelScope.launch {
+            val updatedUiState = _pantryUiState.value.decreaseQuantity(pantryItemId)
+            _pantryUiState.value = updatedUiState
+            val pantryItem = updatedUiState.pantryItems.find { it.id == pantryItemId }
+            pantryItem?.let {
+                if (it.quantity > 0) {
+                    pantryRepository.updateItem(it)
+                } else {
+                    pantryRepository.deleteItem(it)
+                }
+            }
+        }
     }
 }
