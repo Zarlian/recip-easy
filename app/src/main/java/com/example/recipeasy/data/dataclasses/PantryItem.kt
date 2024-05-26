7e78b4bf25efd757f6f1b1461d1750ebc1032fa1
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-26 17:57:53 +0200

    added value for the main ingredient

--- app/src/main/java/com/example/recipeasy/data/dataclasses/PantryItem.kt
+++ app/src/main/java/com/example/recipeasy/data/dataclasses/PantryItem.kt
@@ -10,5 +10,6 @@ data class PantryItem(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,
     val quantity: Int,
-    val image: String = ""
+    val image: String = "",
+    val isMainIngredient: Boolean = false
 )
