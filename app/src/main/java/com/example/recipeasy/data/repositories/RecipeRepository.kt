9b56d959de6636e8411b9b11aeb4c383f497ae03
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-28 14:04:59 +0200

    updated to Mealdetails

--- app/src/main/java/com/example/recipeasy/data/repositories/RecipeRepository.kt
+++ app/src/main/java/com/example/recipeasy/data/repositories/RecipeRepository.kt
@@ -1,9 +1,10 @@
 package com.example.recipeasy.data.repositories
 
+import com.example.recipeasy.data.dataclasses.MealDetails
 import com.example.recipeasy.data.dataclasses.RecipeArticle
 import kotlinx.coroutines.flow.Flow
 
 interface RecipeRepository {
 
-    fun getAllRecipes(): Flow<List<RecipeArticle>>
+    fun getAllRecipes(): Flow<List<MealDetails>>
 }
