4d872f5e86a0874828fa3ef56102aee83eba1419
clint.kindermans <clint.kindermans@student.howest.be>
2024-05-28 14:09:58 +0200

    reworked the dummydata

--- app/src/main/java/com/example/recipeasy/data/DataSource.kt
+++ app/src/main/java/com/example/recipeasy/data/DataSource.kt
@@ -2,10 +2,9 @@ package com.example.recipeasy.data
 
 import androidx.compose.ui.graphics.Color
 import com.example.recipeasy.R
-import com.example.recipeasy.data.dataclasses.IngredientDummy
+import com.example.recipeasy.data.dataclasses.Ingredient
+import com.example.recipeasy.data.dataclasses.MealDetails
 import com.example.recipeasy.data.dataclasses.PantryItem
-//import com.example.recipeasy.data.dataclasses.PantryItem
-import com.example.recipeasy.data.dataclasses.RecipeDummy
 import com.example.recipeasy.data.dataclasses.RecipeArticle
 import com.example.recipeasy.data.dataclasses.ShopArticle
 import com.example.recipeasy.data.dataclasses.ShopItem
@@ -16,109 +15,81 @@ object DataSource {
 
 
     val defaultRecipeList = listOf(
-        RecipeArticle(
-            id = 0,
-            title = "Chicken\n" +
-                    "with roasted\n" +
-                    "vegetables",
-            image = R.drawable.plate_1,
-            color = Color(0xFFEECED3)
+        MealDetails(
+            idMeal = "0",
+            strMeal = "Chicken with roasted vegetables",
+            strInstructions = "Heat the olive oil in a large pan over medium heat. Add the onion, carrot, celery and garlic and cook for 5 minutes. Add the ground beef and cook for 5 minutes. Add the tomato paste, tomatoes, oregano, basil, salt and pepper and cook for 10 minutes. Serve with spaghetti.",
+            strMealThumb = "R.drawable.plate_1",
+            ingredients = listOf(
+                Ingredient("Olive oil", "1 tbsp"),
+                Ingredient("Onion", ""),
+                Ingredient("Carrot", ""),
+                Ingredient("Celery", "1 kg"),
+                Ingredient("Garlic", "1 clover"),
+                Ingredient("Grounded beef", "800 gr"),
+                Ingredient("Tomato paste", "1 tbsp"),
+                Ingredient("Tomatoes", "500 gr"),
+                Ingredient("Oregano", "1 tbsp"),
+                Ingredient("Basil", "1 tbsp"),
+                Ingredient("Salt", "1 kg"),
+                Ingredient("Pepper", "1 tsp")
+            ),
+            difficulty = "Easy",
+            servings = "6",
+            time = "20"
         ),
-        RecipeArticle(
-            id = 1,
-            title = "Spaghetti\n" +
-                    " bolognese",
-            image = R.drawable.plate_2,
-            color = Color(0xFFC9E2FF),
-            recipe = RecipeDummy(
-                time = 20,
-                recipeTitle = "Spaghetti bolognese",
-                difficulty = "Easy",
-                servings = 6,
-                ingredients = listOf(
-                    IngredientDummy("Olive oil", 1, "tbsp"),
-                    IngredientDummy("Onion", 1, ""),
-                    IngredientDummy("Carrot", 3, ""),
-                    IngredientDummy("Celery", 1, "kg"),
-                    IngredientDummy("Garlic", 1, "clover"),
-                    IngredientDummy("Grounded beef", 800, "gr"),
-                    IngredientDummy("Tomato paste", 1, "tbsp"),
-                    IngredientDummy("Tomatoes", 500, "gr"),
-                    IngredientDummy("Oregano", 1, "tbsp"),
-                    IngredientDummy("Basil", 1, "tbsp"),
-                    IngredientDummy("Salt", 1, "kg"),
-                    IngredientDummy("Pepper", 1, "tsp"),
-                ),
-                preparation =
-                "Heat the olive oil in a large pan over medium heat." +
-                        "Add the onion, carrot, celery and garlic and cook for 5 minutes." +
-                        "Add the ground beef and cook for 5 minutes." +
-                        "Add the tomato paste, tomatoes, oregano, basil, salt and pepper and cook for 10 minutes." +
-                        "Serve with spaghetti."
-
-            )
+        MealDetails(
+            idMeal = "1",
+            strMeal = "Spaghetti bolognese",
+            strInstructions = "Pat the salmon fillets dry with paper towels. Season them with salt, pepper, and preferred herbs or spices. Heat a tablespoon of olive oil in a large pan over medium heat. Once the oil is hot, carefully place the seasoned salmon fillets in the pan, skin side down if applicable. Cook for about 3-4 minutes on each side or until they're cooked through and flake easily with a fork. Remove the cooked salmon from the pan and set aside. Prepare your choice of vegetables for steaming by washing and chopping them. Bring water to a boil in a pot fitted with a steamer basket. Place the prepared vegetables in the steamer basket, cover the pot, and steam for about 5-7 minutes or until they're tender but still crisp. Transfer the steamed vegetables to a bowl. Season with a pinch of salt, a dash of olive oil, and maybe a squeeze of lemon juice for added flavor. Arrange the cooked salmon fillets on a plate and serve alongside the steamed vegetables.",
+            strMealThumb = "R.drawable.plate_2",
+            ingredients = listOf(
+                Ingredient("Olive oil", "1 tbsp"),
+                Ingredient("Onion", ""),
+                Ingredient("Carrot", ""),
+                Ingredient("Broccoli", ""),
+                Ingredient("Zucchini", ""),
+                Ingredient("Asparagus", "1 bunch"),
+                Ingredient("Green Beans", "200 g"),
+                Ingredient("Salmon", "500 gr")
+            ),
+            difficulty = "Hard",
+            servings = "2",
+            time = "20"
         ),
-        RecipeArticle(
-            id = 2,
-            title = "Salmon\n" +
-                    "with steamed\n" +
-                    "legumes",
-            image = R.drawable.plate_3,
-            color = Color(0xFFF7CE94),
-            recipe = RecipeDummy(
-                time = 20,
-                recipeTitle = "Salmon",
-                difficulty = "Hard",
-                servings = 2,
-                ingredients = listOf(
-                    IngredientDummy("Olive oil", 1, "tbsp"),
-                    IngredientDummy("Onion", 1, ""),
-                    IngredientDummy("Carrot", 3, ""),
-                    IngredientDummy("Broccoli", 1, ""),
-                    IngredientDummy("Zucchini", 2, ""),
-                    IngredientDummy("Asparagus", 1, "bunch"),
-                    IngredientDummy("Green Beans", 200, "g"),
-                    IngredientDummy("Salmon", 500, "gr"),
-
-                    ),
-                preparation =
-                "Pat the salmon fillets dry with paper towels. Season them with salt, pepper, and preferred herbs or spices." +
-                        "Heat a tablespoon of olive oil in a large pan over medium heat." +
-                        "Once the oil is hot, carefully place the seasoned salmon fillets in the pan, skin side down if applicable. Cook for about 3-4 minutes on each side or until they're cooked through and flake easily with a fork. Remove the cooked salmon from the pan and set aside." +
-                        "Prepare your choice of vegetables for steaming by washing and chopping them." +
-                        "Bring water to a boil in a pot fitted with a steamer basket." +
-                        "Place the prepared vegetables in the steamer basket, cover the pot, and steam for about 5-7 minutes or until they're tender but still crisp." +
-                        "Transfer the steamed vegetables to a bowl. Season with a pinch of salt, a dash of olive oil, and maybe a squeeze of lemon juice for added flavor." +
-                        "Arrange the cooked salmon fillets on a plate and serve alongside the steamed vegetables."
-
-
-            )
+        MealDetails(
+            idMeal = "3",
+            strMeal = "Spaghetti bolognese",
+            strInstructions = "",
+            strMealThumb = "R.drawable.plate_2",
+            ingredients = emptyList(),
+            difficulty = "",
+            servings = "",
+            time = ""
         ),
-        RecipeArticle(
-            id = 3,
-            title = "Spaghetti\n" +
-                    " bolognese",
-            image = R.drawable.plate_2,
-            color = Color(0xFFF7E7A9)
+        MealDetails(
+            idMeal = "9",
+            strMeal = "Spaghetti bolognese",
+            strInstructions = "",
+            strMealThumb = "R.drawable.plate_2",
+            ingredients = emptyList(),
+            difficulty = "",
+            servings = "",
+            time = ""
         ),
-        RecipeArticle(
-            id = 9,
-            title = "Spaghetti\n" +
-                    " bolognese",
-            image = R.drawable.plate_2,
-            color = Color(0xFFF7E7A9)
-        ),
-        RecipeArticle(
-            id = 10,
-            title = "Spaghetti\n" +
-                    " bolognese",
-            image = R.drawable.plate_2,
-            color = Color(0xFFF7E7A9)
-        ),
-
+        MealDetails(
+            idMeal = "10",
+            strMeal = "Spaghetti bolognese",
+            strInstructions = "",
+            strMealThumb = "R.drawable.plate_2",
+            ingredients = emptyList(),
+            difficulty = "",
+            servings = "",
+            time = ""
         )
+    )
 
-    val recipeList: MutableStateFlow<List<RecipeArticle>> = MutableStateFlow(defaultRecipeList)
+    val recipeList: MutableStateFlow<List<MealDetails>> = MutableStateFlow(defaultRecipeList)
 
     val pantryRecipeList = listOf(
         RecipeArticle(
